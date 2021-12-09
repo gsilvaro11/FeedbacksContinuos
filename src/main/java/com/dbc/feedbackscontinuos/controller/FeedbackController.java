cb92d05c5750e66ac9cdec8781a506ecd9115a6a
Ne Guilherme dai Silva <nei.guilherme@dbccompany.com.br>
2021-12-09 19:23:59 -0300

    Adicionando Service, Repository para testes

--- src/main/java/com/dbc/feedbackscontinuos/controller/FeedbackController.java
+++ src/main/java/com/dbc/feedbackscontinuos/controller/FeedbackController.java
@@ -1,10 +1,15 @@
 package com.dbc.feedbackscontinuos.controller;
 
+import com.dbc.feedbackscontinuos.entity.FeedbackEntity;
 import com.dbc.feedbackscontinuos.service.FeedbackService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -12,8 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @RequiredArgsConstructor
 public class FeedbackController {
-
     private final FeedbackService feedbackService;
 
+    @GetMapping("/idFuncionario")
+    public List<FeedbackEntity> list(@RequestParam Integer idFuncionario){
+        return feedbackService.list(idFuncionario);
+    }
 
 }
--- src/main/java/com/dbc/feedbackscontinuos/repository/FeedbackRepository.java
+++ src/main/java/com/dbc/feedbackscontinuos/repository/FeedbackRepository.java
@@ -2,8 +2,13 @@ package com.dbc.feedbackscontinuos.repository;
 
 import com.dbc.feedbackscontinuos.entity.FeedbackEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<FeedbackEntity, Integer> {
+    @Query(value = "SELECT * FROM FEEDBACK f where f.id_funcionario = :idFuncionario", nativeQuery = true)
+    List<FeedbackEntity> findByIdFuncionario(Integer idFuncionario);
 }
