1e5745b9cf79609a733099da54deb087f383da9b
Ne Guilherme dai Silva <nei.guilherme@dbccompany.com.br>
2021-12-09 18:36:50 -0300

    Adicionando DTOS Feedbacks

--- src/main/java/com/dbc/feedbackscontinuos/controller/FeedbackController.java
+++ src/main/java/com/dbc/feedbackscontinuos/controller/FeedbackController.java
@@ -1,4 +1,19 @@
 package com.dbc.feedbackscontinuos.controller;
 
+import com.dbc.feedbackscontinuos.service.FeedbackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/feedbacks")
+@Validated
+@RequiredArgsConstructor
 public class FeedbackController {
+
+    private final FeedbackService feedbackService;
+
+
 }
