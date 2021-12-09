1af98b6a7507755f5e9f9fd27521c2d0cc862408
Ne Guilherme dai Silva <nei.guilherme@dbccompany.com.br>
2021-12-09 18:12:06 -0300

    Adicionando dependencias

--- src/main/java/com/dbc/feedbackscontinuos/security/AuthenticationService.java
+++ src/main/java/com/dbc/feedbackscontinuos/security/AuthenticationService.java
@@ -1,26 +1,28 @@
-package com.dbc.feedbackscontinuos.security;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class AuthenticationService implements UserDetailsService {
-    private final UsuarioService usuarioService;
-
-    @Override
-    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        Optional<UsuarioEntity> usuario = usuarioService.findByLogin(login);
-
-        if(usuario.isPresent()){
-            return usuario.get();
-        }
-
-        return null;
-    }
-}
+//package com.dbc.feedbackscontinuos.security;
+//
+//import com.dbc.feedbackscontinuos.entity.FuncionarioEntity;
+//import com.dbc.feedbackscontinuos.service.FuncionarioService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class AuthenticationService implements UserDetailsService {
+//    private final FuncionarioService funcionarioService;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+//        Optional<FuncionarioEntity> funcionario = funcionarioService.(login);
+//
+//        if(funcionario.isPresent()){
+//            return funcionario.get();
+//        }
+//
+//        return null;
+//    }
+//}
