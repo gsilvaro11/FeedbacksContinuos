56ca010644d7b5171a6b6c9e827182f9bf605498
Ne Guilherme dai Silva <nei.guilherme@dbccompany.com.br>
2021-12-09 14:43:09 -0300

    Adicionando SpringBoot

---
+++ src/main/java/com/dbc/feedbackscontinuos/FeedbackscontinuosApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.feedbackscontinuos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackscontinuosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackscontinuosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbc/feedbackscontinuos/FeedbackscontinuosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbc.feedbackscontinuos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackscontinuosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
