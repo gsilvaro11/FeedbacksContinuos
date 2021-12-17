6dc6c44382ea48895b7203b644252b431638bf44
David <davidsallespro@gmail.com>
2021-12-17 15:30:23 -0300

    Fix na query de busca para tags.

--- src/main/java/com/dbc/feedbackscontinuos/repository/TagsRepository.java
+++ src/main/java/com/dbc/feedbackscontinuos/repository/TagsRepository.java
@@ -15,6 +15,6 @@ public interface TagsRepository extends JpaRepository<TagsEntity, Integer> {
     @Query(value = "select * from tags t where t.status = true", nativeQuery = true)
     List<TagsEntity> findByStatus();
 
-    @Query(value = "SELECT * FROM tags t WHERE upper(t.nome_tag) LIKE %?1%", nativeQuery = true)
+    @Query(value = "SELECT * FROM tags t WHERE t.nome_tag ilike %?1%", nativeQuery = true)
     List<TagsEntity> findTagByName(String tag);
 }
